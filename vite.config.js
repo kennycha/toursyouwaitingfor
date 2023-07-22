a66482a2724b5778408a7485bfa43d8cd5429dba
kennycha <dudqn136@naver.com>
2023-07-22 19:47:50 +0900

    init: init project with basic settings
    - packages
    - docker
    - vite

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from "vite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: {
+    watch: {
+      usePolling: true,
+    },
+    host: true,
+    strictPort: true,
+    port: 5173,
+  },
+});

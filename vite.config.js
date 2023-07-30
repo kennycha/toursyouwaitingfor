092a6c1d4cb763796e955e45bfb29838a50e173f
kennycha <dudqn136@naver.com>
2023-07-30 17:58:42 +0900

    fix: build fix
    - change base in vite.config
    - update workflow yaml file

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/toursyouwaitingfor/",
   server: {
     watch: {
       usePolling: true,
